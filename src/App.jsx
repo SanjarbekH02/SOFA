2100d7a23ef22a52656a6a2807c7adfca57ff9d6
SanjarbekH02 <sanjarbekkhakimov21409@gmail.com>
2025-05-14 13:05:24 +0500

    sofa birinchi versiya tugatildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css';
+import AboutSection from './Components/AboutSection/AboutSection';
+import HeroSection from './Components/Hero/Hero';
+import 'remixicon/fonts/remixicon.css';
+import ServicesSection from './Components/Services/Services';
+import Reviews from './Components/Review/Review';
+import Footer from './Components/Footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      First App
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <Reviews />
+      <Footer />
     </div>
   );
 }
